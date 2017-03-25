244fb6c79db0da4fd85b774c1f70063d022894ac
Jonathan Hudak <hudak.jonathan@gmail.com>
2017-03-25 10:35:16 -0700

    use build-prep branch

--- scripts/buildAndDeploy.sh
+++ scripts/buildAndDeploy.sh
@@ -4,11 +4,14 @@ timestamp() {
   date +"%T"
 }
 
+git branch -d build-deploy
+git checkout -b build-deploy
+
 webpack -p
 ./scripts/build.sh
 git checkout build
 git add -f build
-git commit -am "Build$(date +%s)"
+git commit -am "Build__$(date +%s)"
 git filter-branch -f --prune-empty --subdirectory-filter build
 git push origin build
 git push staging build:master
