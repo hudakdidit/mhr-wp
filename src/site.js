28efea51295d2e64e2438b9ead912dbb53586ddc
Jonathan Hudak <hudak.jonathan@gmail.com>
2017-03-26 14:33:42 -0700

    Added Pjax

--- src/site.js
+++ src/site.js
@@ -1,3 +1,28 @@
 require('./site.css');
+const Pjax = require('pjax');
 
-console.log('hey there');
+console.log("Document initialized:", window.location.href)
+
+document.addEventListener("pjax:send", function() {
+  console.log("Event: pjax:send", arguments)
+})
+
+document.addEventListener("pjax:complete", function() {
+  console.log("Event: pjax:complete", arguments)
+})
+
+document.addEventListener("pjax:error", function() {
+  console.log("Event: pjax:error", arguments)
+})
+
+document.addEventListener("pjax:success", function() {
+  console.log("Event: pjax:success", arguments)
+})
+
+document.addEventListener("DOMContentLoaded", function() {
+  var pjax = new Pjax({
+    elements: 'a',
+    selectors: ['title', '#content']
+  });
+  console.log("Pjax initialized.", pjax)
+})
