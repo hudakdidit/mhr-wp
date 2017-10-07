2d6759d31545400eb80f74a162302d50b337bb6d
Jonathan Hudak <hudak.jonathan@gmail.com>
2017-10-07 21:07:02 +0300

    set active nav module

---
+++ src/js/modules/nav.js
@@ -0,0 +1,27 @@
+const activeClassname = 'active';
+const getCurrentPath = pathname => {
+    const pathParts = pathname.split('/');
+    if (pathParts.length > 1) {
+        return pathParts[pathParts.length - 2];
+    }
+    return '';
+}
+export default {
+    init() {
+        this.navItems = Array.from(document.querySelectorAll('.primary-nav a')).filter(a => a.getAttribute('href') !== '#');
+    },
+
+    setActiveNavItem() {
+        //reset
+        this.navItems.forEach(item => item.classList.remove(activeClassname));
+
+        const currentPath = getCurrentPath(location.pathname);
+        
+        // get current url path
+        this.navItems.forEach(item => {
+            if (getCurrentPath(item.getAttribute('href')) === currentPath) {
+                item.classList.add('active');
+            }
+        });
+    }
+}
