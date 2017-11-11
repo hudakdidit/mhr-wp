19120a7b117249a106af26603ef68f92dab346a7
Jonathan Hudak <hudak.jonathan@gmail.com>
2017-11-11 13:59:32 -0800

    fix galleries for mobile

--- crew.php
+++ crew.php
@@ -8,12 +8,11 @@
 $context = Timber::get_context();
 $post = new TimberPost();
 $context['post'] = $post;
-// $context['crew'] = get_field('crew', 'option');
 
 
 $context['crew'] = get_posts(array(
     "post_type" => "person",
+    "posts_per_page" => -1,
 ));
-// print_r($context);
 Timber::render( array( 'page-' . $post->post_name . '.twig', 'crew.twig' ), $context );
 ?>
