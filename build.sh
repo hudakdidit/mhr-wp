6ba6e2bc01b17d2a68a2146480cedd807aa9653d
Jonathan Hudak <jmhudak@amazon.com>
2019-02-23 14:06:01 -0800

    Update secondary color

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@ webpack -p
 mkdir build
 commit=$(git log -1 --pretty=%B)
 rsync -ruvv --include-from=build-include.txt ./* build
-cd build
-git add .
-git commit -am "Build $(date +%s) of commit: $($commit)"
+# cd build
+# git add .
+# git commit -am "Build $(date +%s) of commit: $($commit)"
